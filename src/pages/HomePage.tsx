d6d6d8d97b2dd5e33fbb2b1645b555f04e6169db
KECO-00 <ksang98@icloud.com>
2024-12-22 15:09:14 +0900

    chore: 성적 미리보기 기능 안내 문구 수정

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -42,7 +42,8 @@ export default function Homepage (props: IHomepageProps) {
         <p><strong>Trinity Parser</strong>가 제공하는 기능은 현재 두 가지가 있습니다.</p>
         <ol>
             <li>특정 교과목의 현재 수강 신청 인원과 수강 제한 인원을 확인할 수 있습니다.</li>
-            <li>인스타에서 한 때 뜨거웠던 비공개 처리 되어있는 성적을 확인할 수 있습니다.</li>
+            <li className="text-canceled">인스타에서 한 때 뜨거웠던 비공개 처리 되어있는 성적을 확인할 수 있습니다.</li>
+            <li>성적 조회와 비공개 처리되어있는 과목의 세부 점수를 확인할 수 있습니다.</li>
         </ol>
         <p>사용자 수에 따라서 부가 기능이 개발될 수 있습니다.</p>
     </div>
