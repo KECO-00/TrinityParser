42b1c72916532d2d8f914b290b7f7ce3dee8fb22
KECO-00 <ksang98@icloud.com>
2025-01-27 15:22:20 +0900

    feat: Route에 /board 추가

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import HomePage from "./pages/HomePage";
 import LoginPage from './pages/LoginPage';
 import GradePage from './pages/GradePage';
 import SugangPage from './pages/SugangPage';
+import BoardPage from './pages/BoardPage';
 import Navbar from './components/Navbar';
 import {Footer} from './components/Footer';
 import './styles/App.css';
@@ -54,6 +55,7 @@ function App() {
             <Route path="/login" element={<LoginPage setLoggedIn={setLoggedIn} />} />
             <Route path="/sugang" element={isLoggedIn ? <SugangPage setLoggedIn={setLoggedIn} /> : <LoginPage setLoggedIn={setLoggedIn} />} />
             <Route path="/grade" element={isLoggedIn ? <GradePage setLoggedIn={setLoggedIn} /> : <LoginPage setLoggedIn={setLoggedIn} />} />
+            <Route path="board" element={ isLoggedIn ? <BoardPage setLoggedIn={setLoggedIn} /> : <LoginPage setLoggedIn={setLoggedIn} />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
           </Routes>
           <Footer />
